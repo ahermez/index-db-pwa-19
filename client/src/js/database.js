15ee4bdeb9a6f4877e9eadebe1632fde2f35e5ed
ahermez <131738383+ahermez@users.noreply.github.com>
2023-09-14 11:47:31 -0700

    added install code and database read functions and update functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const indexPwa = await openDB("PWA", 1);
+  const tx = indexPwa.transaction("PWA", "readwrite");
+  const store = tx.objectStore("PWA");
+  const request = store.put ({
+    id: 1,
+    value: content
+  })
+  const result = await request
+  console.log(result.value)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const indexPwa = await openDB("PWA", 1);
+  const tx = indexPwa.transaction("PWA", "readonly");
+  const store = tx.objectStore("PWA");
+  const request = store.get(1);
+  const results = await request
+  results? console.log(results.value): console.log("no data");
+  return results?.value
+};
 
 initdb();
